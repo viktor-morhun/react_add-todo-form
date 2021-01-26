b4383e3cdc5c7048f7b458d6c806ff7b8a9f0adc
Viktor Morhun <victor.morgun16@gmail.com>
2021-01-26 14:52:47 +0200

    Updates

---
+++ precache-manifest.9f0e9ed7a99c6006396c4613479a8f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b549e4e3799a5daefad1bc60b1bd54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc24fc095a3077b297f1",
+    "url": "./static/css/main.40c984f0.chunk.css"
+  },
+  {
+    "revision": "f70fa610ae01f6e744a6",
+    "url": "./static/js/2.b45151c1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b45151c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc24fc095a3077b297f1",
+    "url": "./static/js/main.3898514a.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
